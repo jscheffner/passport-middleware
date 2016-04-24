6bb27b03139a1091c8ccaddc375b42047de87990
joshmallow <dev.scheffner@outlook.de>
2016-04-24 12:06:53 +0200

    added authentication middleware

---
+++ test/testAuthentication.js
@@ -0,0 +1,42 @@
+﻿"use strict";
+
+var sinon = require('sinon');
+var expect = require('chai').expect;
+var ensureAuthentication = require('../lib/authentication');
+
+describe('Authentication', function () {
+
+    it('should call next if req.isAuthenticated is true', function () {
+        var req, res, next;
+        req = {
+            isAuthenticated: sinon.stub().returns(true)
+        };
+        res = {
+            status: sinon.spy(),
+            end: sinon.spy()
+        };
+        next = sinon.spy();
+            
+        ensureAuthentication(req, {}, next);
+            expect(next.called).to.equal(true);
+            expect(res.status.called).to.equal(false);
+            expect(res.end.called).to.equal(false);
+        });
+        it('should call res.status(401).end() if req.isAuthenticated is false', function () {
+            var req, res, next, statusSpy;
+            req = {
+                isAuthenticated: sinon.stub().returns(false)
+            };
+            res = {};
+            res.status = function () {
+                return this;
+            };
+            res.end = sinon.spy();
+            statusSpy = sinon.spy(res, 'status');
+            next = sinon.spy();
+            ensureAuthentication(req, res, next);
+            expect(next.called).to.equal(false);
+            expect(statusSpy.calledWith(401)).to.equal(true);
+            expect(res.end.called).to.equal(true);
+        });    
+});
